e9c4c7e5d04a236950000e774b9c06ea2d4b63b0
GiridharanS1729 <giridharans1729@gmail.com>
2024-08-05 11:33:43 +0530

    Initial Commit 050820241133

---
+++ 1.Client-Website/src/components/Service.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Services.css';
+
+function Services() {
+  return (
+    <div className="services">
+      <h2>Our Services</h2>
+      <p>This is the services page.</p>
+    </div>
+  );
+}
+
+export default Services;
