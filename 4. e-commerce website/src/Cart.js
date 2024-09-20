676d13c47b0340e07f6629ad3c3157ed0f07ee25
GiridharanS1729 <giridharans1729@gmail.com>
2024-09-21 00:07:06 +0530

    exp4 and ex5

---
+++ 4. e-commerce website/src/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <div className="cart-container">
+      <h1>Your Cart</h1>
+      <p>Your selected products will appear here.</p>
+    </div>
+  );
+}
+
+export default Cart;
---
+++ 4. e-commerce website/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Our Store</h1>
+      <p>Explore our wide range of products!</p>
+    </div>
+  );
+}
+
+export default Home;
